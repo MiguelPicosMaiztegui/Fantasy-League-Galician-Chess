153b1d22c1a838226d4db95b6b3fa487b2ee86ba
MiguelPicosMaiztegui <miguel.picos@gmail.com>
2025-11-27 12:37:13 +0100

    Esqueleto Pagina Web con django

---
+++ fantasychess/accounts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+    return render(request, "home.html")
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("protected")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("protected")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
+
+@login_required
+def protected(request):
+    return render(request, "protected.html")
